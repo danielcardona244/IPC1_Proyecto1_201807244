0200207f0d25a9bf5d4bac8d64b08abea86799fa
danielcardona244 <danielcardonasc@gmail.com>
2024-08-25 23:31:07 -0400

    inicializando el proyecto

---
+++ src/ipc_quimik/IPC_Quimik.java
@@ -0,0 +1,17 @@
+
+package ipc_quimik;
+
+/**
+ *
+ * @author cardo
+ */
+public class IPC_Quimik {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
